95e915e39a457b1a33a9037a9cc47704c0063063
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-11-21 20:47:59 +0530

    add

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Appointment = () => {
   return (
     <div>
-      HomePage
+      
     </div>
   )
 }
 
-export default Home
+export default Appointment
