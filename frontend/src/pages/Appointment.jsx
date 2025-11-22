9fdf85901615e76e8b44c4329892bc4e34bb7a8b
anu123538 <anuththaraimanshi842@gmail.com>
2025-11-22 15:55:49 +0000

    anu1

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
@@ -6,6 +6,15 @@ import { AppContext } from '../context/AppContext'
 const Appointment = () => {
   const {docId}=useParams()
   const {doctors}=useContext(AppContext)
+  const [docInfo,setDocInfo]=useState(null)
+  const fetchDocInfo=async ()=>{
+    const docInfo=doctors.find(doc=>doc._id===docId)
+    setDocInfo(docInfo)
+  }
+  useEffect(()=>{
+  },[doctors,docId])
+
+
   return (
     <div>
       
