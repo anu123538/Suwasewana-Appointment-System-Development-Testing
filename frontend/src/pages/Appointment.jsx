6b8dc006310f1be3258a669ea533bac7b785393e
anu123538 <anuththaraimanshi842@gmail.com>
2025-11-22 15:46:03 +0000

    anu

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
+  const {docId}=useParams()
+  const {doctors}=useContext(AppContext)
   return (
     <div>
       
