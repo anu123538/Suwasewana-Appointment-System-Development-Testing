cfabc98cfc9e48274a288d1071eca1e2f8c00202
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-11-22 18:15:43 +0530

    home added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+      <div>
+          
+          <div>
+              {/*---- Left Section ----*/}
+              <div>
+                  <img src={assets.logo} alt="" />
+                 <p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.</p> 
+              </div>
+
+  {/*---- Center Section ----*/}
+              <div>
+                  
+              </div>
+              
+              {/*---- Right Section ----*/}
+              <div>
+                  
+              </div>
+          </div>
+        </div>
+  )
+}
+              
+
+export default Footer
