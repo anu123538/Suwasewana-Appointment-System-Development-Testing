b04e9f7603be69de5a573816e5652fe84f3b5593
anu123538 <anuththaraimanshi842@gmail.com>
2025-11-22 20:57:39 +0000

    add

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { use } from 'react'
 import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
   const {docId}=useParams()
@@ -10,14 +11,31 @@ const Appointment = () => {
   const fetchDocInfo=async ()=>{
     const docInfo=doctors.find(doc=>doc._id===docId)
     setDocInfo(docInfo)
+    console.log(docInfo)
   }
   useEffect(()=>{
+    fetchDocInfo()
   },[doctors,docId])
 
 
-  return (
+  return docInfo && (
     <div>
-      
+      {/*----------------------Doctor Info----------------------*/}
+      <div>
+        <div>
+          <img src={docInfo.image} alt="" />
+        </div>
+        <div>
+          {/*--------------Doc Info:name,degree,experience---------*/}
+          <p>{docInfo.name}<img src={assets.verified_icon}/></p>
+          <div>
+            <p>{docInfo.degree}-{docInfo.speciality}</p>
+            <button>{docInfo.experience}</button>
+          </div>
+          {/*--------------Doctor About------------------------------*/}
+
+        </div>
+      </div>
     </div>
   )
 }
