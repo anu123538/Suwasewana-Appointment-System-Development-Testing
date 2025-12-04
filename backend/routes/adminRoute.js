15e62bfdd34667ba13694a600985a65538d7e5a6
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-12-04 09:40:23 +0530

    added backedn

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -3,4 +3,7 @@ import { addDoctor } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 
 const adminRouter = express.Router()
+adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
 
+
+export default adminRouter
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
-import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+
 
 
 // app config 
@@ -17,8 +18,7 @@ app.use(express.json())
 app.use(cors())
 
 // api routes 
-app.get('/', (req, res) => {
-    res.send('API Working ')
-})
+app.use('/api/admin', adminRouter)
+
 
 app.listen(port, () => console.log("Server Started", port))
