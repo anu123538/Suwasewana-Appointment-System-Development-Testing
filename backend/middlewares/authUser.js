9de7e979f5dbde4bab8918d4b0a7c6e1033c9a3d
Chamodi Sandeepani <sandeepanichamodi999@gmail.com>
2025-12-17 09:12:30 +0530

    usercontroller

---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+
+// user authentication middleware
+const authUser = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+};
+
+
+export default authUser;
