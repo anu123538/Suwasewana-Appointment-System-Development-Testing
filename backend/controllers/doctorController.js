8791c3c9da2fa4b1a60c424abcf5384daf67511e
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-12-23 23:36:10 +0530

    add changes

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -61,7 +61,7 @@ const loginDoctor = async (req, res) => {
 const appointmentsDoctor = async (req, res) => {
     try {
 
-        const { docId } = req.body
+        const docId = req.doctorId;
         const appointments = await appointmentModel.find({ docId })
         res.json({ success: true, appointments })
 
--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -3,19 +3,20 @@ import jwt from "jsonwebtoken";
 // doctor authentication middleware
 const authDoctor = (req, res, next) => {
   try {
-    const {dtoken} = req.headers;
+    const {dToken} = req.headers;
 
-    if (!dtoken) {
+    if (!dToken) {
       return res.status(401).json({
         success: false,
         message: "Not Authorized. Login Again",
       });
     }
 
-    const token_decoded = jwt.verify(dtoken, process.env.JWT_SECRET);
+    const token_decoded = jwt.verify(dToken, process.env.JWT_SECRET);
 
     // Attach userId to request
-    req.body.docId = token_decoded.id;
+  req.doctorId = decoded.doctorId;
+
 
     next();
   } catch (error) {
