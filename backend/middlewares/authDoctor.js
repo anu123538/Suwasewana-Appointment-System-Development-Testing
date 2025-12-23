7f54b61e604072ce55657d5ffabd853bc3a72a82
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-12-23 23:48:39 +0530

    add this change

--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -3,16 +3,16 @@ import jwt from "jsonwebtoken";
 // doctor authentication middleware
 const authDoctor = (req, res, next) => {
   try {
-    const token = req.headers.dToken; // match frontend
+    const { dtoken } = req.headers;
 
-    if (!token) {
+    if (!dtoken) {
       return res.status(401).json({
         success: false,
         message: "Not Authorized. Login Again",
       });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(dtoken, process.env.JWT_SECRET);
 
     // Attach doctorId to request
     req.body.docId = decoded.doctorId; // match payload
