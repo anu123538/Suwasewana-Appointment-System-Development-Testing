5dd5d99781967629e07a21b14a14ae3eda029e3b
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-12-23 23:46:58 +0530

    asdd this change

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,74 +1,70 @@
-
-import doctorModel from '../models/doctorModel.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import appointmentModel from '../models/appointmentModel.js'
-
-
-
+import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import appointmentModel from "../models/appointmentModel.js";
 
 const changeAvailability = async (req, res) => {
-    try {
-        const { doctorId } = req.body
+  try {
+    const { doctorId } = req.body;
 
-        const docData = await doctorModel.findById(doctorId)
-        await doctorModel.findByIdAndUpdate(doctorId, { available: !docData.available })
+    const docData = await doctorModel.findById(doctorId);
+    await doctorModel.findByIdAndUpdate(doctorId, {
+      available: !docData.available,
+    });
 
-        res.json({ success: true, message: 'Availability status changed successfully' })
-
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
+    res.json({
+      success: true,
+      message: "Availability status changed successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const doctorList = async (req, res) => {
-    try {
-        const doctors = await doctorModel.find({}).select(['-password','-email'])
-        res.json({ success: true, doctors })
-    }
-    catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Doctor login
 const loginDoctor = async (req, res) => {
-    try {
-        const { email, password } = req.body
+  try {
+    const { email, password } = req.body;
 
-        const doctor = await doctorModel.findOne({ email })
-        if (!doctor) {
-            return res.json({ success: false, message: 'Invalid credentials' })
-        }
-        const isMatch = await bcrypt.compare(password, doctor.password)
-
-        if (isMatch) {
-            const token = jwt.sign({ doctorId: doctor._id }, process.env.JWT_SECRET)
-            res.json({ success: true, token })
-        } else {
-            res.json({ success: false, message: 'Invalid credentials' })
-        }
-        
-    }catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
+    const doctor = await doctorModel.findOne({ email });
+    if (!doctor) {
+      return res.json({ success: false, message: "Invalid credentials" });
     }
-}
+    const isMatch = await bcrypt.compare(password, doctor.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ doctorId: doctor._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to get  doctor appointment for doctor panel
 const appointmentsDoctor = async (req, res) => {
-    try {
+  try {
+    const docId = req.body.docId;
+    const appointments = await appointmentModel.find({ docId });
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-        const docId = req.doctorId;
-        const appointments = await appointmentModel.find({ docId })
-        res.json({ success: true, appointments })
-
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
-
-export { changeAvailability,doctorList, loginDoctor, appointmentsDoctor }
+export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor };
