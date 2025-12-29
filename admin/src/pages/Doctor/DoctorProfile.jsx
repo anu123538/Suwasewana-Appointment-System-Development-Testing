151462dc0fa5439b12d964def336574185d57b0c
Saduni Vindhaya Madumali <saduvindhaya@gmail.com>
2025-12-29 21:29:42 +0530

    add changes

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useContext, useEffect } from 'react'
+import { DoctorContext } from '../../context/DoctorContext.jsx'
+import { AppContext } from '../../context/AppContext.jsx'
+
 
 const DoctorProfile = () => {
 
